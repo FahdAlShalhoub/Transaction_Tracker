058686db8b2969fb34ac7f1657c0057ac5cf3078
Fahd <FahdShalhoub@gmail.com>
2020-01-04 16:44:46 +0300

    init messageParser and messageParserTest

--- tests/MessageParserTest.php
+++ tests/MessageParserTest.php
@@ -1,11 +1,23 @@
 <?php 
 
 use PHPUnit\Framework\TestCase;
+use MessageParser as Parser;
 
 final class MessageParserTest extends TestCase 
 {
-    //TODO: write test case before implementation
+   
+    public function testReadMessagesData()
+    {
+        $messages = Parser::readMessagesData("./tests/messagesExample.json");
+        $messagesExample = json_decode(file_get_contents("./tests/messagesExample.json"));
 
-    
-
+        $this->assertIsArray($messages);
+        $this->assertNotEmpty($messages);
+        $this->assertEquals(count($messagesExample),count($messages));
+        
+        foreach($messages as $message){
+            $this->assertIsString($message);
+        }
+        
+    }
 }
